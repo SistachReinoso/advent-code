1803c79f7bb2a96df188e91ad8d495923102981b
Arnau Sistach Reinoso <asistare7@gmail.com>
2024-12-15 08:04:44 +0100

    Day 11, part 1

--- app/src/main/kotlin/com/cucarell/advent/y2024/11.kt
+++ app/src/main/kotlin/com/cucarell/advent/y2024/11.kt
@@ -6,22 +6,41 @@ import kotlin.io.path.useLines
 
 fun main() {
     val file: Path = object {}.javaClass.getResource(
-        "/2024/11/demo"
-        //"/2024/11/input"
+        //"/2024/11/demo"
+        "/2024/11/input"
     )!!.path.let(::Path)
 
-    part1(file) // 36
+    part1(file, 25) // 55312 (25) 26(6)
+        .let(::println)
 }
 
-private fun part1(file: Path) {
-    val list = parser(file)
-    brutus1(list)
-}
+private fun part1(file: Path, steps: Int): Long {
+    val stones = parser(file)
 
-private fun brutus1(list: List<String>) {
-    repeat(6) {
-        TODO()
+    return stones.sumOf { stone ->
+        val a = countStone(stone, steps)
+        println("stone: $stone, steps $steps, total: $a")
+        a
     }
 }
 
-private fun parser(file: Path): List<String> = file.useLines { lines -> lines.first().split("""\s""".toRegex()) }
+private val cache: MutableMap<Pair<Long, Int>, Long> = mutableMapOf()
+private fun withCache(stone: Long, steps: Int, action: (stone: Long, steps: Int) -> (Long)): Long {
+    val a = cache.getOrPut(Pair(stone, steps)) { action(stone, steps) }
+    return a
+}
+
+private fun countStone(stone: Long, steps: Int): Long = withCache(stone, steps) { stone, steps ->
+    if (steps == 0) return@withCache 1
+    if (stone == 0L) return@withCache countStone(1L, steps - 1)
+
+    val string = stone.toString()
+    if (string.length % 2 == 0) {
+        val (left, right) = string.chunked(string.length / 2).map { e -> e.toLong() }
+        return@withCache countStone(left, steps - 1) + countStone(right, steps - 1)
+    }
+    return@withCache countStone(stone * 2024, steps - 1)
+}
+
+private fun parser(file: Path): List<Long> =
+    file.useLines { lines -> lines.first().split("""\s""".toRegex()).map { e -> e.toLong() } }
